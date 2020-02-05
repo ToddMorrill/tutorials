40b77af61d3102a055f1fcfcd2d8e4bc269a1830
Todd Morrill <Todd.Morrill@pwc.com>
2020-02-05 14:33:00 -0500

    python package test

---
+++ pylibrary/pylibrary/main.py
@@ -0,0 +1,11 @@
+from pylibrary.utils import adder
+from pylibrary.cool_feature.feature import subtract
+
+
+def main():
+    subtract()
+    adder()
+    print('complete')
+
+if __name__ == "__main__":
+    main()
---
+++ pylibrary/pylibrary/utils.py
@@ -0,0 +1,2 @@
+def adder():
+    print('2 + 2 = {}'.format(2 + 2))
