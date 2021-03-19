8809247f049db97588ad1f750dd149057f99129a
ToddMorrill <tmorrill12@gmail.com>
2021-03-18 20:17:40 -0400

    start spaCy tutorial

---
+++ spaCy/spaCy_101.py
@@ -0,0 +1,14 @@
+# python3 -m spacy download en_core_web_sm
+import spacy
+from spacy import displacy
+
+nlp = spacy.load("en_core_web_sm")
+doc = nlp("Apple is looking at buying U.K. startup for $1 billion.")
+for token in doc:
+    print(token.text, token.lemma_, token.pos_, token.tag_, token.dep_,
+            token.shape_, token.is_alpha, token.is_stop)
+print()
+for ent in doc.ents:
+    print(ent.text, ent.start_char, ent.end_char, ent.label_)
+
+displacy.serve(doc, style='ent')
